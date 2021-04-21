43f297d3d9441d45342303962263d225119cc8b3
14110145 <waptop4u@gmail.com>
2021-04-21 17:10:39 +0700

    3.1 Category_Update, Delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ const Category = require("../models/categoryModel");
 const categoryController = {
   getCategories: async (req, res) => {
     try {
-      const categories = Category.find();
+      const categories = await Category.find();
       res.json({ categories });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
@@ -11,7 +11,30 @@ const categoryController = {
   },
   createCategory: async (req, res) => {
     try {
-      res.send("checked");
+      const { name } = req.body;
+      const category = await Category.findOne({ name });
+      if (category) return res.status(400).json({ msg: "This category already exist!" });
+      const newCategory = new Category({ name });
+      await newCategory.save();
+
+      return res.status(201).json({ newCategory, msg: "Created a new category!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      await Category.findByIdAndDelete(req.params.id);
+      res.status(200).json({ msg: "Deleted a category!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  updateCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+      await Category.findByIdAndUpdate({ _id: req.params.id }, { name });
+      res.status(200).json({ msg: "Updated a category!" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -11,4 +11,6 @@ router
   .get(categoryController.getCategories)
   .post(auth, authAdmin, categoryController.createCategory);
 
+router.route("/category/:id").delete(auth, authAdmin, categoryController.deleteCategory).put(auth, authAdmin, categoryController.updateCategory);
+
 module.exports = router;
