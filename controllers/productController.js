173998f21fc462308e0c13d334bb06c7c8da3422
14110145 <waptop4u@gmail.com>
2021-04-22 00:11:50 +0700

    5.1 Products_CRUD

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,8 @@ const Product = require("../models/productModel");
 const productController = {
   getProduct: async (req, res) => {
     try {
+      const product = await Product.find();
+      res.status(200).json({ product });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
@@ -31,12 +33,23 @@ const productController = {
   },
   deleteProduct: async (req, res) => {
     try {
+      await Product.findByIdAndDelete(req.params.id);
+      return res.status(200).json({ msg: "Deleted a Product!" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
   },
   updateProduct: async (req, res) => {
     try {
+      const { title, price, description, content, images, category } = req.body;
+      if (!images) return res.status(400).json({ msg: "No image upload!" });
+
+      await Product.findByIdAndUpdate(
+        { _id: req.params.id },
+        { title: title.toLowerCase(), price, description, content, images, category }
+      );
+
+      return res.status(200).json({ msg: "Updated a Product!" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
